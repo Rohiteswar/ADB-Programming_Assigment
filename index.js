8b03aabbc1fa75228d296dd2ac74a767590984dd
Velagapudi Rohiteswar <rohiteswar.velagapudi@gmail.com>
2024-09-30 10:39:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,17 @@ require('dotenv').config();
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-mongoose.connect(process.env.DB_URL, {}).then(() => {
-    console.log("Connected to MongoDB");
-    return server.listen();
-}).then((res) => {
-    console.log(`Server running at ${res.url}`);
-}).catch((err) => console.log(`Error: ${err}`));
+const PORT = process.env.PORT || 4000;
+
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Connected to MongoDB");
+        server.listen(PORT, () => {
+            console.log(`Server running at http://localhost:${PORT}/`);
+        });
+    })
+    .catch((err) => {
+        console.error("MongoDB connection error:", err);
+        process.exit(1);
+    });
+
